ac739988fb5ebe09d70930d9dbb56a073bb771b6
rhtld123 <rhtld123@gmail.com>
2023-03-21 22:44:49 +0900

    feat: 카카오 API 블로그 검색 로직 추가

--- src/main/java/com/example/blogsearch/modules/blogsearch/adapter/out/http/KakaoAdapter.java
+++ src/main/java/com/example/blogsearch/modules/blogsearch/adapter/out/http/KakaoAdapter.java
@@ -1,5 +1,6 @@
 package com.example.blogsearch.modules.blogsearch.adapter.out.http;
 
+import com.example.blogsearch.infrastructure.properties.KakaoProperties;
 import com.example.blogsearch.modules.blogsearch.application.port.out.BlogSearchKakaoPort;
 import com.example.blogsearch.modules.blogsearch.application.port.out.model.BlogSearchKakaoResponse;
 import lombok.RequiredArgsConstructor;
@@ -9,8 +10,12 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class KakaoAdapter implements BlogSearchKakaoPort {
 
+    private final KakaoApiFeignClient kakaoApiFeignClient;
+    private final KakaoProperties kakaoProperties;
+
     @Override
     public BlogSearchKakaoResponse search(String query, String sort, int page, int size) {
-        return null;
+        String accessToken = String.format("KakaoAK %s", kakaoProperties.getToken());
+        return kakaoApiFeignClient.searchBlog(accessToken, query, sort, page, size).getBody();
     }
 }
--- src/main/java/com/example/blogsearch/modules/blogsearch/application/service/BlogSearchService.java
+++ src/main/java/com/example/blogsearch/modules/blogsearch/application/service/BlogSearchService.java
@@ -4,8 +4,16 @@ import com.example.blogsearch.modules.blogsearch.application.port.in.BlogSearchU
 import com.example.blogsearch.modules.blogsearch.application.port.in.model.BlogSearchCommand;
 import com.example.blogsearch.modules.blogsearch.application.port.in.model.BlogSearchResponse;
 import com.example.blogsearch.modules.blogsearch.application.port.out.BlogSearchKakaoPort;
+import com.example.blogsearch.modules.blogsearch.application.port.out.model.BlogSearchKakaoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +23,19 @@ public class BlogSearchService implements BlogSearchUseCase {
 
     @Override
     public BlogSearchResponse search(BlogSearchCommand blogSearchCommand) {
-        return null;
+        BlogSearchKakaoResponse response = blogSearchKakaoPort.search(blogSearchCommand.getKeyword(), blogSearchCommand.getSort().name(), blogSearchCommand.getPage(), blogSearchCommand.getSize());
+        BlogSearchKakaoResponse.Meta meta = response.getMeta();
+        List<BlogSearchKakaoResponse.Documents> documents = response.getDocuments();
+        return BlogSearchResponse.of(meta.getTotalCount(), meta.getPageableCount(), meta.isEnd(), convertDocumentsToContents(documents));
+    }
+
+    private List<BlogSearchResponse.BlogContent> convertDocumentsToContents(List<BlogSearchKakaoResponse.Documents> documents) {
+        return documents.stream()
+                .map(document -> BlogSearchResponse.BlogContent.of(document.getTitle(), document.getContents(), document.getUrl(), document.getBlogname(), document.getThumbnail(), convertDatetime(document.getDatetime())))
+                .collect(Collectors.toList());
+    }
+
+    private LocalDateTime convertDatetime(String datetime) {
+        return LocalDateTime.from(Instant.from(DateTimeFormatter.ISO_DATE_TIME.parse(datetime)).atZone(ZoneId.of("Asia/Seoul")));
     }
 }
