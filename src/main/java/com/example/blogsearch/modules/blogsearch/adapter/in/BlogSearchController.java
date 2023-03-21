dc80e68597c31911b9454ee8aae713d566c958f6
rhtld123 <rhtld123@gmail.com>
2023-03-21 22:26:38 +0900

    feat: Validation 추가

--- src/main/java/com/example/blogsearch/modules/blogsearch/adapter/in/BlogSearchController.java
+++ src/main/java/com/example/blogsearch/modules/blogsearch/adapter/in/BlogSearchController.java
@@ -5,14 +5,15 @@ import com.example.blogsearch.modules.blogsearch.application.port.in.model.BlogS
 import com.example.blogsearch.modules.blogsearch.application.port.in.model.BlogSearchPlatform;
 import com.example.blogsearch.modules.blogsearch.application.port.in.model.BlogSearchResponse;
 import com.example.blogsearch.modules.blogsearch.application.port.in.model.BlogSearchSort;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
 @RestController
 @RequiredArgsConstructor
