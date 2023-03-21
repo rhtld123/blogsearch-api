0d2e7eb6e643a1633bdc3930b8e3eb6f43192f02
rhtld123 <rhtld123@gmail.com>
2023-03-21 22:49:51 +0900

    feat: BaseEntity 추가

---
+++ src/main/java/com/example/blogsearch/infrastructure/common/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.example.blogsearch.infrastructure.common.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, columnDefinition = "datetime")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", columnDefinition = "datetime")
+    private LocalDateTime updatedAt;
+}
