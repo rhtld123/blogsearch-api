1c35ec3f6d3823b6d0d938cfa74da4720f25a6e1
rhtld123 <rhtld123@gmail.com>
2023-03-21 23:48:58 +0900

    feat: 인기 검색어 조회 Service 로직, Response 추가

--- src/main/java/com/example/blogsearch/modules/blogsearch/application/port/in/model/BlogPopluarSearchKeywordResponse.java
+++ src/main/java/com/example/blogsearch/modules/blogsearch/application/port/in/model/BlogPopluarSearchKeywordResponse.java
@@ -2,17 +2,33 @@ package com.example.blogsearch.modules.blogsearch.application.port.in.model;
 
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class BlogPopluarSearchKeywordResponse {
-    private Long count;
-    private String keyword;
 
-    private BlogPopluarSearchKeywordResponse(Long count, String keyword) {
-        this.count = count;
-        this.keyword = keyword;
+    private List<SearchKeyword> data;
+
+    private BlogPopluarSearchKeywordResponse(List<SearchKeyword> data) {
+        this.data = data;
     }
 
-    public static BlogPopluarSearchKeywordResponse of(Long count, String keyword) {
-        return new BlogPopluarSearchKeywordResponse(count, keyword);
+    public static BlogPopluarSearchKeywordResponse from(List<SearchKeyword> data) {
+        return new BlogPopluarSearchKeywordResponse(data);
+    }
+
+    @Getter
+    public static class SearchKeyword {
+        private Long count;
+        private String keyword;
+
+        private SearchKeyword(Long count, String keyword) {
+            this.count = count;
+            this.keyword = keyword;
+        }
+
+        public static SearchKeyword of(Long count, String keyword) {
+            return new SearchKeyword(count, keyword);
+        }
     }
 }
--- src/main/java/com/example/blogsearch/modules/blogsearch/application/service/GetBlogPopularSearchKeywordService.java
+++ src/main/java/com/example/blogsearch/modules/blogsearch/application/service/GetBlogPopularSearchKeywordService.java
@@ -2,15 +2,28 @@ package com.example.blogsearch.modules.blogsearch.application.service;
 
 import com.example.blogsearch.modules.blogsearch.application.port.in.GetBlogPopularSearchKeywordUseCase;
 import com.example.blogsearch.modules.blogsearch.application.port.in.model.BlogPopluarSearchKeywordResponse;
+import com.example.blogsearch.modules.blogsearch.application.port.out.GetBlogPopularSearchKeywordPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class GetBlogPopularSearchKeywordService implements GetBlogPopularSearchKeywordUseCase {
 
+    private final GetBlogPopularSearchKeywordPort getBlogPopularSearchKeywordPort;
+
     @Override
     public BlogPopluarSearchKeywordResponse getBlogPopularSearchKeyword() {
-        return null;
+        return BlogPopluarSearchKeywordResponse.from(getSearchKeywords());
+    }
+
+    private List<BlogPopluarSearchKeywordResponse.SearchKeyword> getSearchKeywords() {
+        return getBlogPopularSearchKeywordPort.getBlogPopularSearchKeywordPort()
+                .stream()
+                .map(data -> BlogPopluarSearchKeywordResponse.SearchKeyword.of(data.getSearchCount(), data.getKeyword()))
+                .collect(Collectors.toList());
     }
 }
