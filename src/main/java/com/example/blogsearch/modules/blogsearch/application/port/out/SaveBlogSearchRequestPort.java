b96449332ff80cda67b0bf55333cd1b6831cad0d
rhtld123 <rhtld123@gmail.com>
2023-03-21 22:57:48 +0900

    feat: 검색 기록 저장 Port 추가

---
+++ src/main/java/com/example/blogsearch/modules/blogsearch/application/port/out/SaveBlogSearchRequestPort.java
@@ -0,0 +1,7 @@
+package com.example.blogsearch.modules.blogsearch.application.port.out;
+
+import com.example.blogsearch.modules.blogsearch.adapter.out.persistence.entity.BlogSearchRequestEntity;
+
+public interface SaveBlogSearchRequestPort {
+    void save(BlogSearchRequestEntity entity);
+}
