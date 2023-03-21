876b0174a68de8d1288c7c3764732cdbe231e08e
rhtld123 <rhtld123@gmail.com>
2023-03-21 21:50:12 +0900

    initial commit

---
+++ src/main/java/com/example/blogsearch/BlogsearchApiApplication.java
@@ -0,0 +1,15 @@
+package com.example.blogsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class BlogsearchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogsearchApiApplication.class, args);
+    }
+
+}
