f79f256ddcb79500d697f19c17fd037dc555aed6
rhtld123 <rhtld123@gmail.com>
2023-03-22 09:20:02 +0900

    test: KakaoAdapterTest 추가

---
+++ src/test/java/com/example/blogsearch/modules/blogsearch/adapter/out/http/KakaoAdapterTest.java
@@ -0,0 +1,64 @@
+package com.example.blogsearch.modules.blogsearch.adapter.out.http;
+
+import com.example.blogsearch.infrastructure.properties.KakaoProperties;
+import com.example.blogsearch.modules.blogsearch.application.port.out.model.BlogSearchKakaoResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class KakaoAdapterTest {
+
+    @InjectMocks
+    KakaoAdapter kakaoAdapter;
+    @Mock
+    KakaoApiFeignClient kakaoApiFeignClient;
+    @Mock
+    KakaoProperties kakaoProperties;
+
+    @Test
+    void 조회_성공() {
+        //given
+        when(kakaoProperties.getToken()).thenReturn("token");
+        when(kakaoApiFeignClient.searchBlog(anyString(), anyString(), anyString(), anyInt(), anyInt())).thenReturn(ResponseEntity.ok().body(createResponse()));
+        //when
+        BlogSearchKakaoResponse response = kakaoAdapter.search("keyword", "ACCURACY", 1, 10);
+        //then
+        BlogSearchKakaoResponse.Meta meta = response.getMeta();
+        assertEquals(100L, meta.getTotalCount());
+        assertEquals(10L, meta.getPageableCount());
+        assertFalse(meta.isEnd());
+        assertTrue(response.getDocuments().size() == 10);
+    }
+
+    @Test
+    void kakao_properties가_null일_때_NullPointerException() {
+        //given
+        when(kakaoProperties.getToken()).thenReturn(null);
+        //when
+        //then
+        assertThrows(NullPointerException.class, () -> kakaoAdapter.search("keyword", "ACCURACY", 1, 10));
+    }
+
+    private BlogSearchKakaoResponse createResponse() {
+        BlogSearchKakaoResponse.Meta meta = BlogSearchKakaoResponse.Meta.of(100L, 10L, false);
+        List<BlogSearchKakaoResponse.Documents> documents = new ArrayList<>();
+        IntStream.range(0, 10).forEach(i -> {
+            BlogSearchKakaoResponse.Documents document = BlogSearchKakaoResponse.Documents.of("keyword" + i, "contents", "url", "blog", "thumbnail", "");
+            documents.add(document);
+        });
+        return BlogSearchKakaoResponse.of(meta, documents);
+    }
+}
